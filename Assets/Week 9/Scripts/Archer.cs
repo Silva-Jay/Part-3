4f5af0932e34d0f634516fea70ee87a16ba818f3
Sophia Grasman <grasman@shernet.sheridancollege.ca>
2024-03-14 14:25:17 -0400

    Task 1 complete - archer and merchant child classes made!

---
+++ Assets/Week 9/Scripts/Archer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Apple;
+
+public class Archer : Villager
+{
+    public GameObject arrowPrefab;
+    public Transform spawnPoint;
+    public float delay = 0.466f;
+
+    protected override void Attack()
+    {
+        destination = transform.position;
+        base.Attack();
+        Invoke("SpawnArrow", delay);
+    }
+
+    void SpawnArrow()
+    {
+        Instantiate(arrowPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
